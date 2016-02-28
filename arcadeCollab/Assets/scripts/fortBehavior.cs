e55a24f954813df1bfaf8dff18005f3b103e1148
loldi <genu4ldi@gmail.com>
2016-02-28 12:08:41 -0500

    final commits
    should be the final version

---
+++ arcadeCollab/Assets/scripts/fortBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fortBehavior : MonoBehaviour {
+
+	public float fortHealth = 100.0f;
+
+
+	void Update () {
+		if (fortHealth <= 0.0f) {
+			Destroy (this.gameObject);
+			global.playState = 2;
+		}
+	}
+	
+	void BlowUp (float damageTaken) {
+		fortHealth -= damageTaken;
+		Debug.Log ("Fort Hit");
+	}
+}
---
+++ arcadeCollab/Assets/scripts/global.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class global {
+
+	public static int playState = 0;
+
+	public static int wallPos = Random.Range(1,4);
+
+	public static void AddExplosionForce(this Rigidbody2D body, float explosionForce, Vector3 explosionPosition, float explosionRadius)
+	{
+		var dir = (body.transform.position - explosionPosition);
+		float wearoff = 1 - (dir.magnitude / explosionRadius);
+		body.AddForce(dir.normalized * explosionForce * wearoff);	
+	}
+}
